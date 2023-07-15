b0afb99783aced952eea454eb2a4ff5b3c5c06c9
Rikkuthedev <Macqyntajz.gonzales@gmail.com>
2023-07-15 17:43:04 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True # Enable the members intent
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command()
+async def whatip(ctx):
+    # Open the file and read the text
+    with open('ip.txt', 'r') as file:
+        text = file.read()
+        
+    # Send the text to the channel
+    await ctx.send(text)
+
+# Replace YOUR_BOT_TOKEN with your bot token
+bot.run('MTA4NTUxNDEzMDEyMDc4NTkyMA.GWWV5u._0aC-hMN5td_233DC0OoR8vNeRU8AZa1N-Meww')
